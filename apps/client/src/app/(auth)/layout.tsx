13094cc5c3aeee66c6d59a3723c723abb0c5b658
Erik van Dam <erikvandam93@gmail.com>
2023-05-01 15:24:09 +0200

    feat: added check to login page to redirect to dashboard when a user is already authenticated

--- apps/client/src/app/(auth)/layout.tsx
+++ apps/client/src/app/(auth)/layout.tsx
@@ -1,9 +1,20 @@
 import { ReactNode } from 'react'
+import { getAmplifyWithSSRContext } from '@/lib'
+import { getCurrentUser } from '@/lib/session'
+import { redirect } from 'next/navigation'
 
 interface AuthLayoutProps {
   children: ReactNode
 }
 
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default async function AuthLayout({ children }: AuthLayoutProps) {
+  const SSR = getAmplifyWithSSRContext()
+
+  const currentUser = await getCurrentUser(SSR.Auth)
+
+  if (currentUser) {
+    return redirect('/dashboard')
+  }
+
   return <div className="flex min-h-full flex-1">{children}</div>
 }
