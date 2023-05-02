b7f9da2dffb573c65dc19e16351feb7a7acb48a5
Erik van Dam <erikvandam93@gmail.com>
2023-05-02 18:28:38 +0200

    refactor: moved web application to vercel

--- apps/client/src/app/(auth)/layout.tsx
+++ apps/client/src/app/(auth)/layout.tsx
@@ -1,20 +1,13 @@
-import { ReactNode } from 'react'
-import { getAmplifyWithSSRContext } from '@/lib'
-import { getCurrentUser } from '@/lib/session'
-import { redirect } from 'next/navigation'
+import { ReactNode, Suspense } from 'react'
 
 interface AuthLayoutProps {
   children: ReactNode
 }
 
-export default async function AuthLayout({ children }: AuthLayoutProps) {
-  const SSR = getAmplifyWithSSRContext()
-
-  const currentUser = await getCurrentUser(SSR.Auth)
-
-  if (currentUser) {
-    return redirect('/dashboard')
-  }
-
-  return <div className="flex min-h-full flex-1">{children}</div>
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <Suspense fallback={null}>
+      <div className="flex min-h-full flex-1">{children}</div>
+    </Suspense>
+  )
 }
